eb596ab0d5f86862da9614f7efef76210b25632c
Shylesh S <37534898+shyleshsunnithan@users.noreply.github.com>
2020-04-10 20:19:10 +0530

    Update 18.Stack_array.c

--- 18.Stack_array.c
+++ 18.Stack_array.c
@@ -15,7 +15,7 @@ int main()
     top=-1;
     printf("\n Enter the size of STACK : ");
     scanf("%d",&n);
-    printf("\n\t STACK OPERATIONS USING ARRAY");
+    printf("\n\t STACK OPERATIONS USING ARRAY ");
     printf("\n\t--------------------------------");
     printf("\n\t 1.PUSH\n\t 2.POP\n\t 3.DISPLAY\n\t 4.EXIT");
     do
